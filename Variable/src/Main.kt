f3591a908c07b15fea6c194d1c2fb54009f56424
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2024-12-10 23:08:39 +0600

    Add files via upload

---
+++ Variable/src/Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val firstName = "Neel "
+    val lastName = "Islam"
+    val fullName = firstName + lastName
+
+    val birthyear = 2001
+    val cg = 3.14
+
+    val name_user = "Sneha"
+    println("Hello " + name_user+" Welcome to my program")
+
+
+
+    println("Ï am "+fullName)
+    println(birthyear)
+    println(cg)
+
+    val x = 5
+    val y = 6
+    println(x + y)
+    println(x * y)
+    println(x / y)
+    println(x - y)
+
+    val myNum: Short = 5000
+    println(myNum)
+
+}
