be3911b007d7ed2ad28172bacfba7825ad68a115
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2024-12-12 19:22:01 +0600

    Add files via upload

---
+++ Operators/src/Main.kt
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+val x = 1;val y = 2
+val z = 3
+val a = 4
+
+
+ println("Reminder of " + x + " and " + y+ " is : "+ x%y)
+ if (x == 1 && y == 2) println("This is x == 1 && y == 2")
+    if (x == 2 || y == 2) println("This is x == 2 || y == 2")
+    if (!(x == 2 && y == 2)) println("This is!(x == 2 && y == 2)")
+}
